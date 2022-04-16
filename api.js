ce381a63226689cd63b5237e4f1729160c345460
justthedoctor <justthedoctor.931@gmail.com>
2022-04-16 13:00:46 -0400

    Update api.js
    fixed errors and some spacing

--- api.js
+++ api.js
@@ -4,6 +4,10 @@ const request = require('request');
 const fs = require('fs');
 var app = express();
 
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
@@ -25,10 +29,9 @@ app.get('/votingbooth/1', (req, res) => {
          return res.status(500).json({ type: 'error', message: error });
        }
        var info = JSON.parse(body);
-       res.write('<!DOCTYPE html><html><head><title>Pandacoin Peer Asset - Voting Booth</title></head><body><div style="margin:0 auto;text-align:center;width:50%;"><div>Pandacoin Peer Asset Votes<br>Experimenting with Pandacoin peer asset voting<br>Does Voting with Pandacoin peer assets seem like a good idea?<br>Send a peer asset Votes Tokens to one of the following pandacoin addresses to vote.<br>For Yes send to: P8mQQqQbymmR99zUb4ZnvRyxcEQMYhurHe<br>For No send to: PNm95MJ9pGye7B4hAJKHVjX4fuWWEUik1R<br></div></div><div style="margin:0 auto;width:50%;"><div style="text-align:center;float:left;">P8mQQqQbymmR99zUb4ZnvRyxcEQMYhurHe</br> Yes Votes Tokens Count:' + info.objects[0].value + '</div>');
+       res.write('<!DOCTYPE html><html><head><title>Pandacoin Peer Asset - Voting Booth</title></head><body style="background-image:url(' + "'http://cryptodepot.org/jback.jpg');" +'background-repeat:no-repeat; background-attachment:fixed;background-position:center;"><div style="margin:0 auto;text-align:center;width:50%;"><div style="margin-top:60px;">Pandacoin Peer Asset Votes<br>Experimenting with Pandacoin peer asset voting<br>Does Voting with Pandacoin peer assets seem like a good idea?<br>Send a peer asset Votes Tokens to one of the following pandacoin addresses to vote.<br>For Yes send to: P8mQQqQbymmR99zUb4ZnvRyxcEQMYhurHe<br>For No send to: PNm95MJ9pGye7B4hAJKHVjX4fuWWEUik1R<br></div></div><div style="margin:0 auto;width:45%;"><div style="text-align:center;float:left;">P8mQQqQbymmR99zUb4ZnvRyxcEQMYhurHe</br> Yes Votes Tokens Count: ' + info.objects[0].value + '</div>');
      }
    )
-
    request(
      { url: 'https://pnddev.digitalpandacoin.org:5555/restless/v1/balances?q=%7B%22filters%22:[%7B%22name%22:%22account%22,%22op%22:%22like%22,%22val%22:%22PNm95MJ9pGye7B4hAJKHVjX4fuWWEUik1R%%22%7D]%7D'},
      (error, response, body) => {
@@ -36,9 +39,10 @@ app.get('/votingbooth/1', (req, res) => {
          return res.status(500).json({ type: 'error', message: error });
        }
        var info = JSON.parse(body);
-       console.log(info);
        res.write('<div style="text-align:center; float: right;">PNm95MJ9pGye7B4hAJKHVjX4fuWWEUik1R </br> No Votes Tokens Count: ' + info.objects[0].value + '</div></div></body></html>');
-       res.end();
+       sleep(2000).then(() => {
+         res.end();
+       });
      }
    )
  });
